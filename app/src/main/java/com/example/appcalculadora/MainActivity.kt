d1819c9e214fa1831f0c4add08c692e31618b07c
FERNURIBE <laurafernandauribe@gmail.com>
2025-02-27 15:33:47 -0500

    Parte 2 - Laura Uribe

--- app/src/main/java/com/example/appcalculadora/MainActivity.kt
+++ app/src/main/java/com/example/appcalculadora/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.appcalculadora
 
 import android.icu.text.DecimalFormat
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -9,32 +11,68 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
-    val suma= "+"
-    val resta= "-"
-    val multiplicacion= "*"
-    val division= "/"
+    val suma = "+"
+    val resta = "-"
+    val multiplicacion = "*"
+    val division = "/"
+    val porcentaje = "%"
 
     var operacionActual = ""  //Aqui se ve el resultado
 
-    var primerNumero :Double =Double.NaN
-    var segundoNumero :Double =Double.NaN
+    var primerNumero: Double = Double.NaN
+    var segundoNumero: Double = Double.NaN
 
-    lateinit var tvTemp :TextView
-    lateinit var tvResult:TextView
+    lateinit var tvTemp: TextView
+    lateinit var tvResult: TextView
 
-    lateinit var formatoDecimal :DecimalFormat
+    lateinit var formatoDecimal: DecimalFormat
 
-    override fun onCreate(savedInstanceState: Bundle?){
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R. layout.activity_main)
+        setContentView(R.layout.activity_main)
 
-        formatoDecimal = DecimalFormat("#.##########") //Estos # indican el patron del formato decimal, cada # es un número
+        formatoDecimal =
+            DecimalFormat("#.##########") //Estos # indican el patron del formato decimal, cada # es un número
         tvTemp = findViewById(R.id.tvTemp)
         tvResult = findViewById(R.id.tvResult)
-         }
-
-    //Aqui sigue el codigooooooooooooooooooooooooooooooooooooooooooooooooooooooo
-    fun cambiarOperador ()
-
-
     }
+
+
+    fun cambiarOperador(b: View) {
+        calcular()
+        val boton: Button = b as Button
+        if (boton.text.toString().trim() == "÷") {
+            operacionActual = "/"
+        } else if (boton.text.toString().trim() == "x") {
+            operacionActual = "*"
+        } else {
+            operacionActual = boton.text.toString().trim()
+        }
+
+        tvResult.text = formatoDecimal.format(primerNumero) + operacionActual
+        tvTemp.text = ""
+    }
+
+    fun calcular(){
+        if (primerNumero.toString()!="NaN"){
+            segundoNumero = tvTemp.text.toString().toDouble()
+            tvTemp.text=""
+
+            when(operacionActual){
+                "+"-> primerNumero = (primerNumero+segundoNumero)
+                "-"-> primerNumero = (primerNumero-segundoNumero)
+                "*"-> primerNumero = (primerNumero*segundoNumero)
+                "/"-> primerNumero = (primerNumero/segundoNumero)
+                "%"-> primerNumero = (primerNumero%segundoNumero)
+            }
+        }else{
+
+            primerNumero = tvTemp.text.toString().toDouble()
+        }
+    }
+
+    fun seleccionarNumero(b: View){
+        val boton:Button = b as Button
+        tvTemp.text = tvTemp.text.toString() + boton.text.toString()
+    }
+}
